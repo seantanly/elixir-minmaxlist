a6bc1f454a1e6f9be1f6f8430ce3cbafb949e2f3
Sean Tan <sean.tan.ly@gmail.com>
2015-11-03 16:29:08 +0800

    Update code/doc style to follow Elixir master.

--- lib/minmaxlist.ex
+++ lib/minmaxlist.ex
@@ -3,50 +3,28 @@ defmodule Minmaxlist do
   Adds `min_list_by/2`, `max_list_by/2` and `min_max_list_by/2` that's not in `Enum`.
   """
 
+  import Enum, only: [reduce: 3]
+
   @type t :: Enumerable.t
   @type element:: any
 
   @doc """
-  Returns list of entries that gives the minimum value as calculated by the given function.
-  Returns empty list if the collection is empty.
+  Returns list of the biggest elements in the enumerable as calculated by the given function.
 
-      iex> Minmaxlist.min_list_by(["a", "b", "aa", "bb", "bbb", "aaa"], fn(x) -> String.length(x) end)
-      ["a", "b"]
-      iex> Minmaxlist.min_list_by([], fn(x) -> String.length(x) end)
-      []
-  """
-  @spec min_list_by(t, (element -> any)) :: list
-  def min_list_by(collection, fun) do
-    result = collection |> Enum.reduce(:first, fn
-      entry, :first ->
-        fun_entry = fun.(entry)
-        {[entry], fun_entry}
-      entry, {acc_entries, acc_fun_entry} ->
-        fun_entry = fun.(entry)
-        cond do
-          fun_entry == acc_fun_entry -> {[entry|acc_entries], fun_entry}
-          fun_entry < acc_fun_entry -> {[entry], fun_entry}
-          true -> {acc_entries, acc_fun_entry}
-        end
-    end)
-    case result do
-      :first -> []
-      _ -> result |> elem(0) |> Enum.reverse
-    end
-  end
+  Returns empty list if the enumerable is empty.
 
-  @doc """
-  Returns list of entries that gives the maximum value as calculated by the given function.
-  Returns empty list if the collection is empty.
+  ## Examples
 
       iex> Minmaxlist.max_list_by(["a", "b", "aa", "bb", "bbb", "aaa"], fn(x) -> String.length(x) end)
       ["bbb", "aaa"]
+
       iex> Minmaxlist.max_list_by([], fn(x) -> String.length(x) end)
       []
+
   """
   @spec max_list_by(t, (element -> any)) :: list
-  def max_list_by(collection, fun) do
-    result = collection |> Enum.reduce(:first, fn
+  def max_list_by(enumerable, fun) do
+    result = enumerable |> reduce(:first, fn
       entry, :first ->
         fun_entry = fun.(entry)
         {[entry], fun_entry}
@@ -60,22 +38,61 @@ defmodule Minmaxlist do
     end)
     case result do
       :first -> []
-      _ -> result |> elem(0) |> Enum.reverse
+      _ -> result |> elem(0) |> :lists.reverse
     end
   end
 
   @doc """
-  Returns a tuple with two lists of entries that give minimum & maximum values respectively as calculated by the given function.
-  Returns a tuple of two empty lists if the collection is empty.
+  Returns list of the smallest elements in the enumerable as calculated by the given function.
+
+  Returns empty list if the enumerable is empty.
+
+  ## Examples
+
+      iex> Minmaxlist.min_list_by(["a", "b", "aa", "bb", "bbb", "aaa"], fn(x) -> String.length(x) end)
+      ["a", "b"]
+
+      iex> Minmaxlist.min_list_by([], fn(x) -> String.length(x) end)
+      []
+
+  """
+  @spec min_list_by(t, (element -> any)) :: list
+  def min_list_by(enumerable, fun) do
+    result = enumerable |> reduce(:first, fn
+      entry, :first ->
+        fun_entry = fun.(entry)
+        {[entry], fun_entry}
+      entry, {acc_entries, acc_fun_entry} ->
+        fun_entry = fun.(entry)
+        cond do
+          fun_entry == acc_fun_entry -> {[entry|acc_entries], fun_entry}
+          fun_entry < acc_fun_entry -> {[entry], fun_entry}
+          true -> {acc_entries, acc_fun_entry}
+        end
+    end)
+    case result do
+      :first -> []
+      _ -> result |> elem(0) |> :lists.reverse
+    end
+  end
+
+  @doc """
+  Returns a tuple with list of smallest elements and list of biggest elements in the enumerable as calculated by the given function.
+
+  Returns a tuple of two empty lists if the enumerable is empty.
+
+  ## Examples
 
       iex> Minmaxlist.min_max_list_by(["a", "b", "aa", "bb", "bbb", "aaa"], fn(x) -> String.length(x) end)
       {["a", "b"], ["bbb", "aaa"]}
+
       iex> Minmaxlist.min_max_list_by([], fn(x) -> String.length(x) end)
       {[], []}
+
   """
   @spec min_max_list_by(t, (element -> any)) :: {list, list}
-  def min_max_list_by(collection, fun) do
-    result = collection |> Enum.reduce(:first, fn
+  def min_max_list_by(enumerable, fun) do
+    result = enumerable |> reduce(:first, fn
       entry, :first ->
         fun_entry = fun.(entry)
         {{[entry], fun_entry}, {[entry], fun_entry}}
@@ -95,7 +112,8 @@ defmodule Minmaxlist do
     end)
     case result do
       :first -> {[], []}
-      {result_min, result_max} -> {result_min |> elem(0) |> Enum.reverse, result_max |> elem(0) |> Enum.reverse}
+      {result_min, result_max} -> {result_min |> elem(0) |> :lists.reverse, result_max |> elem(0) |> :lists.reverse}
     end
   end
+
 end
